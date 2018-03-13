e593ad94a502ebf8b20d1216762d3d03b27c4942
epasse <erik_j_m@hotmail.com>
2018-03-13 20:55:22 +0000

    Open and close chrome

--- homerun.py
+++ homerun.py
@@ -1,1 +1,24 @@
-print("Starting")
+#/usr/bin/env python
+import subprocess, time
+from os.path import abspath, dirname, join
+
+def start_chrome():
+	print("Starting chrome")
+	html_path = abspath(join(dirname(__file__), './html/test.html'))
+	cmd = ['chromium-browser', html_path]
+	subprocess.Popen(cmd)
+
+def turn_off_chrome():
+	print("Turns of chrome")
+	cmd = ['pkill', 'chromium']
+	subprocess.call(cmd)
+	print("Done")
+	
+if __name__ == "__main__":
+	print("Starting")
+	start_chrome()
+	time.sleep(25)
+	turn_off_chrome()
+	
+
+

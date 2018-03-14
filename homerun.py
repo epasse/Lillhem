3a690cffee7f412deb63824db8d35884fb79734a
epasse <erik.passe@zenuity.com>
2018-03-14 18:35:36 +0100

    Add chrome Ubuntu

--- homerun.py
+++ homerun.py
@@ -1,24 +1,37 @@
 #/usr/bin/env python
-import subprocess, time
+import subprocess, time, os
 from os.path import abspath, dirname, join
 
+if 'x86_64' in os.uname():
+    BROWSER = 'google-chrome'
+    BROWSER_KILL = 'chrome'
+else: 
+    BROWSER = 'chromium-browser'  # RPi        
+    BROWSER_KILL = 'chromium'
+
 def start_chrome():
-	print("Starting chrome")
-	html_path = abspath(join(dirname(__file__), './html/test.html'))
-	cmd = ['chromium-browser', html_path]
-	subprocess.Popen(cmd)
+    print("Starting chrome")
+    html_path = abspath(join(dirname(__file__), './html/test.html'))
+    cmd = [BROWSER, html_path, '--noerrdialogs', '--kiosk', '--incognito', '--disable-translate'] # '--ignore-certificate-errors'
+    subprocess.Popen(cmd)
 
 def turn_off_chrome():
-	print("Turns of chrome")
-	cmd = ['pkill', 'chromium']
-	subprocess.call(cmd)
-	print("Done")
+    print("Turns of chrome")
+    cmd = ['pkill', BROWSER_KILL]
+    subprocess.call(cmd)
+    print("Done")
 	
 if __name__ == "__main__":
-	print("Starting")
-	start_chrome()
-	time.sleep(25)
-	turn_off_chrome()
+    print("Starting")
+    start_chrome()
+    time.sleep(25)
+    turn_off_chrome()
 	
 
+#* Create a script to replace the standard desktop environment.
+#```
+## ~/fullscreen.sh
+#unclutter &
+#matchbox-window-manager &
 
+#sed -i 's/"exited_cleanly": false/"exited_cleanly": true/' ~/.config/chromium-browser Default/Preferences
